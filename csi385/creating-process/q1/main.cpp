26fea010c80acd2dfa93e021e663b249ae357006
Rei Armenia <lazerzes@users.noreply.github.com>
2017-09-12 18:03:12 -0400

    Add files via upload

---
+++ csi385/creating-process/q1/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	system("ps aux");
+	system("mkdir something");
+	system("cd something");
+	system("kill 1057");
+
+	system("pause");
+
+	return 0;
+
+}
