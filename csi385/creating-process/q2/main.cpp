0cf3f550210dc8b5a63d69862fe97bb446e8f6f7
Matthew James Harrison <mharrison@fedora.black>
2017-09-12 22:11:25 -0400

    Simplified input system. Fork/exec is now fully functional.

--- csi385/creating-process/q2/main.cpp
+++ csi385/creating-process/q2/main.cpp
@@ -7,45 +7,49 @@ using namespace std;
 
 const string BIN_PATH = "/bin/";
 
-string remove_spaces(string data)
-{
-    size_t position = string::npos;
-    while (data.length() > 0)
-    {
-        position = data.find(' ');
-        if (position == string::npos)
-        {
-            break;
-        }
-        else
-        {
-            data.erase(position, 1);
-        }
-    }
-    return data;
-}
-
 int main()
 {
     pid_t pid;
-    string user_input = "";
-    string program_name = "";
-    string program_arguments = "";
-    string *program_arguments_array;
-    size_t position = string::npos;
+    string data = "";
+    size_t length = 0;
+    size_t argument_count = 0;
+    char *program;
+    char **arguments;
 
-    getline(cin, user_input);
-    position = user_input.find(' ');
-    if (position == string::npos)
+    // Get the path of the program
+    cout << "What program would you like to run? ";
+    cin >> data;
+    data = BIN_PATH + data;
+    length = data.length();
+    program = new char[length+1];
+    data.copy(program, length);
+    program[length+1] = '\0';
+
+    // Get argc
+    cout << "How many arguments? ";
+    cin >> data;
+    for (size_t i = 0; i < data.length(); i++)
     {
-        program_name = user_input;
-        program_arguments = "";
+        if (!isdigit(data[i]))
+        {
+            cout << "Error: NaN\n";
+            exit(1);
+        }
     }
-    else
+    argument_count = stoi(data);
+    arguments = new char*[argument_count+1];
+
+    // Get arguments
+    for (size_t i = 0; i < argument_count; i++)
     {
-        program_name = user_input.substr(0, position);
-        program_arguments = user_input.substr(position+1, user_input.length());
+        cout << "Please enter argument [" << i+1 << "]: ";
+        cin >> data;
+        length = data.length();
+        arguments[i] = new char[length+1];
+        data.copy(arguments[i], length);
+        arguments[i][length+1] = '\0';
     }
+    arguments[argument_count] = NULL;
 
     pid = fork();
     if (pid > 0)
@@ -56,7 +60,7 @@ int main()
     else if (pid == 0)
     {
         // Child process
-        //execv(BIN_PATH + program_name);
+        execv(program, arguments);
         exit(1);
     }
     else
@@ -65,6 +69,12 @@ int main()
         perror("Fork failed with the following error");
         exit(1);
     }
+
+    for (size_t i = 0; i < argument_count; i++)
+    {
+        delete arguments[i];
+    }
+    delete [] arguments;
 
     return 0;
 }
