6ea2f01bc9036112195c13cfb6e433c515f00004
Matthew James Harrison <mharrison@fedora.black>
2017-09-24 22:27:25 -0400

    Separated Q1 and Q2

---
+++ csi385/lab3/q1/main.cpp
@@ -0,0 +1,68 @@
+/*       Authors: Rei Armenia, Matthew James Harrison
+ *         Class: CSI-385 Operating Systems Architecture
+ *    Assignment: Creating Processes
+ *      Due Date: September 24, 2017
+ *
+ * Description:
+ *   This C++ program runs "ps -ax" via a child process whose output has been
+ *   redirected to a pipe.
+ *
+ * Certication of Authenticity:
+ *   I certify that this is entirely my own work, except where I have given
+ *   fully-documented references to the work of others. I understand the
+ *   definition and consequences of plagiarism and acknowledge that the
+ *   assessor of this assignment may, for the purpose of assessing this
+ *   assignment:
+ *     -  Reproduce this assignment and provide a copy to another member of
+ *        academic staff; and/or
+ *     -  Communicate a copy of this assignment to a plagiarism checking service
+ *        (which may then retain a copy of this assignment on its database for
+ *        the purpose of future plagiarism checking)
+ ******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    FILE *fpipe = NULL;
+    char *command = (char*)"ps -ax";
+    char *buf = NULL;
+    size_t rb = 0;
+    size_t pos = 0;
+    int i = 0;
+    string line = "";
+    
+    if (!(fpipe = (FILE*)popen(command, "r")))
+    {
+        perror("Problems with pipe");
+        exit(1);
+    }
+    
+    while ( (rb = getline(&buf, &rb, fpipe)) != -1 )
+    {
+        line = buf;
+        
+        if (i == 0)
+        {
+            pos = line.find("COMMAND");
+        }
+        else if (i == 1)
+        {
+            line.replace(pos, rb-1, "REDACTED\n");
+        }
+        
+        cout << line;
+
+        rb = 0;
+        free(buf);
+        buf = NULL;
+        i++;
+    }
+    pclose(fpipe);
+}
+
