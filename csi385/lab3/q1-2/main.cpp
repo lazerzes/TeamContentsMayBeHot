f39ff55eee8d81ade24ded141a2bfcb362cef9fe
Matthew James Harrison <mharrison@fedora.black>
2017-09-24 21:30:32 -0400

    Q3 works perfectly

--- csi385/lab3/q1-2/main.cpp
+++ csi385/lab3/q1-2/main.cpp
@@ -4,7 +4,8 @@
  *      Due Date: September 24, 2017
  *
  * Description:
- *   This C++ program...
+ *   This C++ program runs "ps -ax" via a child process whose output has been
+ *   redirected to a pipe.
  *
  * Certication of Authenticity:
  *   I certify that this is entirely my own work, except where I have given
@@ -27,133 +28,78 @@
 
 using namespace std;
 
-const int CHILD_ARGC = 2;
-const string CHILD_ARGV[CHILD_ARGC] = 
-{
-    "/bin/ps",
-    "-ax"
-};
-const bool VERBOSE = true;
-
 int main()
 {
-    // process management
+    char *argv_ps[] = 
+    {
+        "/bin/ps",
+        "-ax",
+        NULL
+    };
+    char buf;
+    size_t i = 0;
+    string line = "";
+    pid_t pid;
     int pipefd[2];
-    char **cargv;
-    char *command = (char*)"ps -ax";
-    
-    // string parsing
-    char *buf = NULL;
-    size_t rb = -1;
     size_t pos = string::npos;
-    size_t len = string::npos;
-    pid_t cpid;
-    int i;
-    string data;
+    int rb = -1;
     
-    if (VERBOSE) { cout << "Creating pipe" << endl; }
-    if ( pipe(pipefd) == -1 )
+    // Create a new pipe
+    if (pipe(pipefd) == -1)
     {
         perror("Failed to pipe");
-        exit(1);
     }
-    
-    if (VERBOSE) { cout << "Forking process" << endl; }
-    cpid = fork();
-    if (cpid == -1)
+
+    // Fork program into two processes
+    if ((pid = fork()) == -1)
     {
         perror("Failed to fork");
-        exit(1);
     }
-    // Child process will write to pipe
-    else if (cpid == 0)
-    {
-        if (VERBOSE) { cout << "Child: Fork successful" << endl; }
     
-        // Close read fd from pipe
-        close(pipefd[0]);
+    // Child process
+    if (pid == 0)
+    {
+        close(pipefd[0]); // Close STDIN
+        dup2(pipefd[1], 1); // Redirect STDOUT to pipe output
         
-        if (VERBOSE) { cout << "Child: Closed unused read end" << endl; }
-
-        // Allocate heap for list of arguments
-        cargv = (char**)malloc(sizeof(char*) * CHILD_ARGC);
+        execvp(argv_ps[0], argv_ps);
         
-        if (VERBOSE) { cout << "Child: Allocated memory for argv" << endl; }
-        
-        // Allocate heap for program path and load data it
-        data = CHILD_ARGV[0];
-        len = data.length();
-        cargv[0] = (char*)malloc(len+1);
-        data.copy(cargv[0], len);
-        cargv[0][len+1] = '\0';
-        
-        if (VERBOSE) { cout << "Child: Allocated memory for argv[0]" << endl; }
-        
-        // Allocate heap for program argument(s) and load it
-        data = CHILD_ARGV[1];
-        len = data.length();
-        cargv[0] = (char*)malloc(len+1);
-        data.copy(cargv[1], len);
-        cargv[1][len+1] = '\0';
-
-        if (VERBOSE) { cout << "Child: Allocated memory for argv[1]" << endl; }
-
-        // Replace STDOUT with write fd from pipe
-        dup2(pipefd[1], STDOUT_FILENO);
-
-        if (VERBOSE) { cout << "Child: Executing program" << endl; }
-
-        execv(cargv[0], cargv);
-        
-        perror("Failed to execv");
+        perror("Failed to execute program");
         exit(1);
     }
-    // Parent process will read from pipe
+    // Parent process
     else
     {
-        // Close write fd from pipe
-        close(pipefd[1]);
+        close(pipefd[1]); // Close pipe input
         
-        if (VERBOSE) { cout << "Parent: Initiated read loop" << endl; }
+        // Read pipe output character-by-character
         i = 0;
-        while ( rb = read(pipefd[0], &buf, 256) > 0 )
+        while ( (rb = read(pipefd[0], &buf, 1)) == 1 )
         {
-            if (buf == NULL)
+            // Append character to line
+            line.push_back(buf);
+            
+            // Detect end of line
+            if (buf == '\n')
             {
-                perror("Failed to read pipe");
-                exit(1);
+                // Scan header
+                if (i == 0)
+                {
+                    pos = line.find("COMMAND");
+                }
+                // Redact first process name
+                else if (i == 1)
+                {
+                    line.replace(pos, line.length(), "REDACTED\n");
+                }
+            
+                // Display and clear the line
+                cout << line;
+                line = "";
+                
+                i++;
             }
-            
-            data = buf;
-            
-            // If first line, find the column where "COMMAND" starts
-            if (i == 0)
-            {
-                if (VERBOSE) { cout << "Parent: Identified headers" << endl; }
-                pos = data.find("COMMAND");
-            }
-            // If second line, replace the command string
-            else if (i == 1)
-            {
-                if (VERBOSE) { cout << "Parent: Redacting" << endl; }
-                data.replace(pos, rb-1, "REDACTED\n");
-            }
-            
-            cout << data;
-            
-            rb = -1;
-            free(buf);
-            buf = NULL;
-            i++;
-            
-            if (VERBOSE) { cout << "Parent: Read " << i << " lines" << endl; }
         }
-        
-        close(pipefd[0]);
-        
-        if (VERBOSE) { cout << "Parent: Waiting for child to end" << endl; }
         wait(NULL);
     }
-    
-    return 0;
 }
