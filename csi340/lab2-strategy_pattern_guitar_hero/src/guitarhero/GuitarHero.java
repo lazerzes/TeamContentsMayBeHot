89196e0350f1c7f6ba5581a66f5a033c2ffab544
Armenia <rei.armenia@champlain.edu>
2017-09-28 12:14:21 -0400

    Finish project

--- csi340/lab2-strategy_pattern_guitar_hero/src/guitarhero/GuitarHero.java
+++ csi340/lab2-strategy_pattern_guitar_hero/src/guitarhero/GuitarHero.java
@@ -1,8 +1,36 @@
 package guitarhero;
 
+import guitarhero.containers.*;
+import guitarhero.guitar.*;
+import guitarhero.solo.*;
+
 public class GuitarHero {
   
   public void main(String[] args){
+	  
+	  GameCharacter player1 = new GameCharacterAngus("Player One", new FenderTelecasterGuitar(), new GuitarFireSolo());
+	  GameCharacter player2 = new GameCharacterHendrix("Player Two", new GibsonFlyingVGuitar(), new GuitarSmashSolo());
+	  GameCharacter player3 = new GameCharacterSlash("Player Three", new GibsonSGGuitar(), new StageJumpSolo());
+	  
+	  player1.display();
+	  player1.playGuitar();
+	  player1.playSolo();
+	  
+	  player2.display();
+	  player2.playGuitar();
+	  player2.playSolo();
+	  
+	  player3.display();
+	  player3.playGuitar();
+	  player3.playSolo();
+	  
+	  player3.setPlayerGuitar(new FenderTelecasterGuitar());
+	  player3.playGuitar();
+	  
+	  player1.setPlayerSolo(new GuitarSmashSolo());
+	  player1.playSolo();
+	  
+	  
   }
   
 }
