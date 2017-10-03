5e1f219d7475b7fb5ac382362c27517ed2d47621
Armenia <rei.armenia@23-148.champlain.edu>
2017-10-03 12:12:36 -0400

    Rei Lab 5 initialization

---
+++ csi385/lab5/main_rei.cpp
@@ -0,0 +1,21 @@
+// file: main.cpp
+// Assignment: Lab 5 - Threads
+// Authors: Rei Armenia & Matthew Harrison
+//
+/*
+ * Requirements:
+ *   1. Write a program that creates an array of 1000 threads. Each one prints Hello.
+ *   2. Modify the first program so that each thread prints "hello %i" where 'i' is the thread number
+ *   3. Write a program that creates an arroy of 1000 ints. Create an array of 1000 threads where
+ *		a. Each Thread will write 0 to the even indices
+ *		b. Each Thread will write 1 to the odd indices
+*/
+
+#include <iostream>
+#include <thread>
+
+int main(){
+
+   //Placeholder text
+
+}
