0337c7a5e283507496f35656d2bc021959c27810
Matthew James Harrison <mjsonofharry@gmail.com>
2017-10-04 20:12:06 -0400

    Implemented Item

--- csi340/lab3-observer_pattern-auction/src/auction/Item.java
+++ csi340/lab3-observer_pattern-auction/src/auction/Item.java
@@ -1,4 +1,13 @@
 package auction;
 
 public class Item {
+	public String name;
+	public double basePrice;
+	public double marketValue;
+	
+	public Item(String name, double basePrice, double marketValue) {
+		this.name = name;
+		this.basePrice = basePrice;
+		this.marketValue = marketValue;
+	}
 }
