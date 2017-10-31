0e1d534972f8348c2d7f5d0ac6cb18e43a749343
lazerzes <lazerzes@gmail.com>
2017-10-31 12:23:35 -0400

    class update
    finding free space done

--- csi385/memorymanagement/cpp-version/main.cpp
+++ csi385/memorymanagement/cpp-version/main.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
+#include <vector>
+
+#define uint unsigned int
 
 using namespace std;
 
+struct Node {
+	
+	Node* previous;
+	Node* next;
+
+	uint start;
+	uint size;
+
+	string name;
+
+	Node(uint start, uint size, string name) {
+		this->start = start;
+		this->size = size;
+		this->name = name;
+
+	}
+
+};
+
+class MemoryManager {
+
+public:
+	uint size;
+	Node* head;
+	
+	MemoryManager(uint size) {
+		this->size = size;
+		head = new Node(0, this->size, "unused");
+	}
+
+	vector<Node*> findEmptySpaceForProcess(uint size) {
+		vector<Node*> list;
+
+		Node* temp = head;
+		while (temp != NULL) {
+			if (temp->name == "unused" && size <= temp->size) {
+				list.push_back(temp);
+			}
+		}
+
+		return list;
+
+	}
+
+	void allocate(string name, uint size) {
+
+		vector<Node*> list = this->findEmptySpaceForProcess(size);
+
+	}
+
+
+};
+
 int main() {
+
+
 
 	return 0;
 
